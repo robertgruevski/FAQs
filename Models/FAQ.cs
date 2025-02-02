fe8c08146471582e967a05c0097b1de81d30b22f
robertgruevski <robertgruevski@outlook.com>
2025-02-02 13:05:02 -0500

    Add project files.

---
+++ Models/FAQ.cs
@@ -0,0 +1,13 @@
+﻿namespace FAQs.Models
+{
+	public class FAQ
+	{
+		public int FAQId { get; set; }
+		public string Question { get; set; } = string.Empty;
+		public string Answer { get; set; } = string.Empty;
+		public string CategoryId { get; set; } = string.Empty; // Foreign Key 
+		public Category Category { get; set; } = null!; // Navigation Property
+		public string TopicId { get; set; } = string.Empty;
+		public Topic Topic { get; set; } = null!; // Navigation Property
+	}
+}
---
+++ Models/Topic.cs
@@ -0,0 +1,9 @@
+﻿namespace FAQs.Models
+{
+	public class Topic
+	{
+		public string TopicID { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+
+	}
+}
