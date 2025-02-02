efd2b66543b8f4586ed7d4d313a6db93e67be347
robertgruevski <robertgruevski@outlook.com>
2025-02-02 13:37:47 -0500

    Bringing data to the view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,21 @@
 using System.Diagnostics;
 using FAQs.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FAQs.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private FaqsContext context { get; set; }
+        public HomeController(FaqsContext ctx)
         {
-            _logger = logger;
+            this.context = ctx;
         }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            List<FAQ> faqs = context.FAQs.Include(f => f.Category).Include(f => f.Topic).OrderBy(f => f.Question).ToList();
+            return View(faqs);
         }
     }
 }
