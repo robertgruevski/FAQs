66388cbcf5b94941e382c4c2b4b2d0d0fa5ca156
robertgruevski <robertgruevski@outlook.com>
2025-02-02 13:26:24 -0500

    Add-Migration Update-Database EF

---
+++ Migrations/20250202182508_DbCreation.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FAQs.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.TopicId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FAQs",
+                columns: table => new
+                {
+                    FAQId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQs", x => x.FAQId);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "TopicId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { "gen", "General" },
+                    { "hist", "History" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Topics",
+                columns: new[] { "TopicId", "Name" },
+                values: new object[,]
+                {
+                    { "asp", "ASP.NET Core" },
+                    { "blz", "Blazor" },
+                    { "ef", "Entity Framework" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FAQs",
+                columns: new[] { "FAQId", "Answer", "CategoryId", "Question", "TopicId" },
+                values: new object[,]
+                {
+                    { 1, "ASP.NET Core is a free and open-source web framework, and the next generation of ASP.NET, developed by Microsoft.", "gen", "What is ASP.NET Core?", "asp" },
+                    { 2, "ASP.NET Core 1.0 was released on June 7, 2016.", "hist", "When was ASP.NET Core released?", "asp" },
+                    { 3, "Blazor is a free and open-source web framework that enables developers to create web user interfaces (UI) based on components, using C# and HTML. It is being developed by Microsoft, as part of the ASP.NET Core web app framework.", "gen", "What is Blazor?", "blz" },
+                    { 4, "Blazor was released on May 14, 2020.", "hist", "When was Blazor released?", "blz" },
+                    { 5, "Entity Framework is an open-source ORM framework for .NET applications support by Microsoft.", "gen", "What is Entity Framework?", "ef" },
+                    { 6, "Entity Framework 1.0 was released in 2008.", "gen", "When was Entity Framework released?", "ef" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_CategoryId",
+                table: "FAQs",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_TopicId",
+                table: "FAQs",
+                column: "TopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FAQs");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+        }
+    }
+}
