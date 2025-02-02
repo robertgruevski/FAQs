a213a8a788ec07939496bf1d0f836c5974eb8121
robertgruevski <robertgruevski@outlook.com>
2025-02-02 13:24:36 -0500

    Database connection string and dependency injection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using FAQs.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FaqsContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("FAQsCS")));
 
 var app = builder.Build();
 
